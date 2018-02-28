f92f50488855ff22d67091424a5261adbcf5e5eb
Borkhalenko <borkhalenko@gmail.com>
2018-02-28 11:20:56 +0200

    Deleted some files.

--- Dt_CRM/Controllers/HomeController.cs
+++ Dt_CRM/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Dt_CRM.Models;
 
 namespace Dt_CRM.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ViewResult Index()
         {
             DateTime dt = DateTime.Now;
             ViewBag.currentTime = dt;
+            return View("Hello");
+        }
+
+        [HttpPost]
+        public ViewResult Index(PointJobRecord pjRecord)
+        {
+            PointJobDatabase.AddRecord(pjRecord);
             return View("Hello");
         }
         //public IActionResult Index()
